40cc20815ec69e52e9f42224e2a4f06c5cbfd9a7
miguel5612 <miguelangelcu@ufps.edu.co>
2020-03-28 20:13:49 -0500

    Updated readme

--- Arduino_Respirator.ino
+++ Arduino_Respirator.ino
@@ -9,48 +9,40 @@ void setup() {
   pinMode(dirPin, OUTPUT);
 }
 void loop() {
-  // Set the spinning direction clockwise:
-  digitalWrite(dirPin, HIGH);
-  // Spin the stepper motor 1 revolution slowly:
-  for (int i = 0; i < stepsPerRevolution; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(2000);
-  }
-  delay(1000);
-  // Set the spinning direction counterclockwise:
+  giroCompletoContinuo();
+  //medioGiro();
+}
+
+void giroCompletoContinuo()
+{
   digitalWrite(dirPin, LOW);
-  // Spin the stepper motor 1 revolution quickly:
-  for (int i = 0; i < stepsPerRevolution; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
+  for(int i=0; i<=50; i++)
+  {
+    analogWrite(stepPin, 5);
+    delay(10);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
+    delay(10);      
   }
-  delay(1000);
-  // Set the spinning direction clockwise:
-  digitalWrite(dirPin, HIGH);
-  // Spin the stepper motor 5 revolutions fast:
-  for (int i = 0; i < 5 * stepsPerRevolution; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(500);
-  }
-  delay(1000);
-  // Set the spinning direction counterclockwise:
-  digitalWrite(dirPin, LOW);
-  //Spin the stepper motor 5 revolutions fast:
-  for (int i = 0; i < 5 * stepsPerRevolution; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(500);
-  }
-  delay(1000);
+}
+void medioGiro()
+{
+  
+    digitalWrite(dirPin, LOW);
+    for(int i=0; i<=50; i++)
+    {
+      analogWrite(stepPin, 5);
+      delay(10);
+      digitalWrite(stepPin, LOW);
+      delay(10);      
+    }
+    delay(2000);
+    digitalWrite(dirPin, HIGH);
+    for(int i=0; i<=50; i++)
+    {
+      analogWrite(stepPin, 5);
+      delay(10);
+      digitalWrite(stepPin, LOW);
+      delay(10);      
+    }
+    delay(1500);
 }
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Arduino_Respirator
 Arduino low cost respirator to assist coronavirus crisis.
+# Objective
+To design a low cost/ high sofisticated respirator using local materials
+# Materials
+## Electronics:
+* Arduino nano v3
+* Driver DRV8825
+* Nema motor
+* Emergency button
+* 12v @1A power supply
+## Mechanic:
+* Base material for case and supports (Acrylic and steel)
+* viela drive system
+## Neumatic:
+* Ambu
+* anti return valve
+* Accessories
+# Team
+1. Miguel Angel Califa Urquiza
+2. Yersson Ramiro Carrillo Amado
+3. Pedro Jesús Gonzalez Torres
+4. Cesar Salazar
+5. Yeison Ferney Osorio buitrago
+6. Alber Iván Sánchez Álvarez
+7. Juan Enrique Vargas López
+8. Johan jazer orduz Molina
