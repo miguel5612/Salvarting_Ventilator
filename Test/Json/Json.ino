a6d7d48619075ee8a8a17edd119113caf5e6a9c9
miguel5612 <miguelangelcu@ufps.edu.co>
2020-06-26 14:35:59 -0500

    First compiled json.ino

--- Test/Json/Json.ino
+++ Test/Json/Json.ino
@@ -1,8 +1,38 @@
+/*
+Date: 26.06.2020
+Autor: Miguel Califa
+Objective:
+    Send a JSON using serialPort without libraries and the most simplified
+    as posible.
+Base: json.json
+*/
+String jsonObject = "";
 void setup()
 {
-
+    Serial.begin(9600);
 }
 void loop()
 {
-    
+    jsonObject = '\
+    {\
+        "Nombre_Ventilador": "Salvarting-Demo-0001",\
+        "Fecha_Fabricacion": "2020/06/26",\
+        "Volumen_Ambu": "600",\
+        "Peep": "60%",\
+        "FiO2": "Manual - Valvulas",\
+        "Relacion_IE": "1:1",\
+        "PIP": "30%",\
+        "Modo_Control": "CV-Control por volumen",\
+        "Alarmas": "No - habilitado",\
+        "Frecuencia_Respiratoria": 15,\
+        "Valvula_Alivio_Presion": "Ambu - Default",\
+        "Sensor_Flujo": "YF-S201",\
+        "Flujo": 1.23,\
+        "Unidad_Flujo": "m3",\
+        "Sensor_Presion": "MPX5010GP",\
+        "Presion": 20,\
+        "Unidad_Presion": "KPa"\
+    }';
+    Serial.println(jsonObject);
+    delay(1000);
 }
