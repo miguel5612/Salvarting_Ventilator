d8e6d87bcf5d6dffa3d018ea61686462d920b6ca
miguel5612 <miguelangelcu@ufps.edu.co>
2020-08-19 07:45:53 -0500

    Added test valves

--- Test/valves/valves.ino
+++ Test/valves/valves.ino
@@ -1,8 +1,23 @@
-#define valveInsp 13
-#define valveExp 14
+#define valveInsp 8
+#define valveExp 9
 
 void setup()
 {
     pinMode(valveInsp, OUTPUT);
     pinMode(valveExp, OUTPUT);
+    digitalWrite(valveInsp, LOW);
+    digitalWrite(valveExp, LOW);
+}
+
+void loop()
+{
+    digitalWrite(valveInsp, LOW);
+    digitalWrite(valveExp, HIGH);
+    
+    delay(1000);
+
+    digitalWrite(valveInsp, HIGH);
+    digitalWrite(valveExp, LOW);
+
+    delay(1000);
 }
