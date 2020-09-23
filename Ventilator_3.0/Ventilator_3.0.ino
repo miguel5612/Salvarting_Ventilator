fa399c9dd9dcd0a07b04ccb381be2b3ad49fc3f5
miguel5612 <miguelangelcu@ufps.edu.co>
2020-09-22 22:22:21 -0500

    New version has multitask version :D

--- Ventilator_3.0/Ventilator_3.0.ino
+++ Ventilator_3.0/Ventilator_3.0.ino
@@ -1,5 +1,13 @@
 /*
  * 
+ * Ventilator software version 3.0 
+ * Created By: Miguel Califa
+ * Start: 09.2020
+ * Motivation: Implementing FreeRTOS we can do all tasks managed by OS.
+ * Support: miguelangel5612@gmail.com
+ * 
+ * 
+
 // Pins for the RAMPS board
 const int X_STEP_PIN         = 54;
 const int X_DIR_PIN          = 55;
@@ -13,6 +21,10 @@ const int Z_STEP_PIN         = 46;
 const int Z_DIR_PIN          = 48;
 const int Z_ENABLE_PIN       = 62;  // Active LOW
  */
+
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>  // add the FreeRTOS functions for Semaphores (or Flags).
+#include <AccelStepper.h>  // Stepper / servo library with step pulse / dir interface
 
 #define pin_Stepper_DIR        55
 #define pin_Stepper_Step       54
@@ -33,7 +45,6 @@ const int Z_ENABLE_PIN       = 62;  // Active LOW
 #define pot1 A5 // Relacion Inspiracion/Expiracion
 #define pot2 A11 // PIP 
 #define pot3 A12 // Frecuencia respiratoria
-
 #define selector A10
 
 // Configs
@@ -55,8 +66,6 @@ short sel = 0; //0 - Apagado; 1 - Control volumen; 2 - Control Presion
 int temp = 0, volume, PIP, freq;
 float IE;
 
-#include <AccelStepper.h>  // Stepper / servo library with step pulse / dir interface
-AccelStepper stepper(1, pin_Stepper_Step, pin_Stepper_DIR);
 
 double motorSpeed = 800;
 double maxRecorridoMotor = 900;
@@ -68,12 +77,123 @@ int cantidadCiclos = 0;
 
 const int distanceToEndstopMinZ = 160;
 const int distanceToTouchAmbu = 0;
+const int PressureSensorPin = A3;
+const int FlowSensorPin = 32;
 
+// Declare a mutex Semaphore Handle which we will use to manage the Serial Port.
+// It will be used to ensure only only one Task is accessing this resource at any time.
+SemaphoreHandle_t xSerialSemaphore;
+AccelStepper stepper(1, pin_Stepper_Step, pin_Stepper_DIR);
+
+// define two Tasks for DigitalRead & AnalogRead
+void TaskDigitalRead( void *pvParameters );
+void TaskAnalogRead( void *pvParameters );
+
+// the setup function runs once when you press reset or power the board
 void setup() {
+
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  ArduinoSetup();
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
+  
+  // Semaphores are useful to stop a Task proceeding, where it should be paused to wait,
+  // because it is sharing a resource, such as the Serial port.
+  // Semaphores should only be used whilst the scheduler is running, but we can set it up here.
+  if ( xSerialSemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
+  {
+    xSerialSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore we will use to manage the Serial Port
+    if ( ( xSerialSemaphore ) != NULL )
+      xSemaphoreGive( ( xSerialSemaphore ) );  // Make the Serial Port available for use, by "Giving" the Semaphore.
+  }
+
+  // Now set up Tasks to run independently.
+  xTaskCreate(
+    TaskVentilatePatient
+    ,  "Ventilate_Patient"
+    ,  128  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+  xTaskCreate(
+    TaskLeerPots
+    ,  "Read controls"
+    ,  128  // Stack size
+    ,  NULL
+    ,  2  // Priority
+    ,  NULL );
+
+  xTaskCreate(
+    TaskMonitorVars
+    ,  "Monitor vars"
+    ,  128  // Stack size
+    ,  NULL
+    ,  3  // Priority
+    ,  NULL );
+  // Now the Task scheduler, which takes over control of scheduling individual Tasks, is automatically started.
+  
+}
+
+void loop()
+{
+  // Empty. Things are done in Tasks.
+}
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+void TaskMonitorVars( void *pvParameters __attribute__((unused)) )  // This is a Task.
+{
+
+  for (;;)
+  {
+    monitorVars();
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
+
+void TaskLeerPots( void *pvParameters __attribute__((unused)) )  // This is a Task.
+{
+
+  for (;;)
+  {
+    leerPots();
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
+
+void TaskVentilatePatient( void *pvParameters __attribute__((unused)) )  // This is a Task.
+{
+
+  for (;;)
+  {
+    loopVentilator();
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
+
+void monitorVars()
+{
+  serialShow("Flow Sensor lecture: ");
+  serialShow(String(digitalRead(FlowSensorPin)));
+  serialShow("Pressure Sensor lecture: ");
+  serialShow(String(analogRead(PressureSensorPin)));  
+  vTaskDelay(32);  // one tick delay (15ms) in between reads for stability
+  vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+}
+void ArduinoSetup()
+{
+  
   // put your setup code here, to run once:
   Serial.begin(SERIAL_BAUD);
   pinMode(electroVExpiracion, OUTPUT);
   pinMode(electroVInspiracion, OUTPUT);
+  pinMode(FlowSensorPin, INPUT);
+  pinMode(PressureSensorPin, INPUT);
   pinMode(minZendstop, INPUT_PULLUP);
   
   pinMode(pin_Stepper_DIR, OUTPUT);
@@ -97,6 +217,8 @@ void setup() {
   Serial.println("Init started");
   
   digitalWrite(pin_Stepper_DIR, HIGH);     // active (inverted)
+
+  /* DESCOMENTAR, IMPORTANTEEEE
   while(!z0reached())
   {
       stepper.setSpeed(-motorSpeed);
@@ -109,42 +231,43 @@ void setup() {
   alejarseFinalCarrera();
   
   delay(150);
+  */
 }
 
-void loop() {
-  digitalWrite(electroVExpiracion, LOW);
+void loopVentilator()
+{
+   digitalWrite(electroVExpiracion, LOW);
   digitalWrite(electroVInspiracion, HIGH);
-  Serial.println("Inspiracion");  
-  //Serial.println(stepper.currentPosition());
+  serialShow("Inspiracion");  
+  //serialShowln(stepper.currentPosition());
+  serialShow("Avance lento");
   while (stepper.currentPosition() < (maxRecorridoMotor - distanceToEndstopMinZ - distanceToTouchAmbu))  {
     stepper.setSpeed(motorSpeed);
     stepper.runSpeed();
-    Serial.println("Avance lento");
-    //Serial.println(stepper.currentPosition());
-    delay(delayDriverResponse);
+    //serialShowln(stepper.currentPosition());
+    vTaskDelay(delayDriverResponse);
   }
   //while(1);
-  delay(tiempoInspiracionExpiracion*1000);
+  vTaskDelay(tiempoInspiracionExpiracion*1000);
 
   digitalWrite(electroVExpiracion, HIGH);
   digitalWrite(electroVInspiracion, LOW);
-  Serial.println("Expiracion");
+  serialShow("Expiracion");
+  serialShow("Reversa rapida");
   while ((stepper.currentPosition() > distanceToEndstopMinZ) & (!z0reached()))   {
     stepper.setSpeed(-(motorSpeed/relacionInspiracionExpiracion));
     stepper.runSpeed();
-    delay(delayDriverResponse);
-    Serial.println("Reversa rapida");
-    //Serial.println(stepper.currentPosition());
+    vTaskDelay(delayDriverResponse);
+    //serialShowln(stepper.currentPosition());
   }
 
   delay(delayToRemoveInercia); // Elimina la inercia que lleve el brazo
   if(z0reached()) alejarseFinalCarrera();
   //delay(tiempoInspiracionExpiracion*1000);
   cantidadCiclos++;
-  Serial.print("Ciclos: ");
-  Serial.println(cantidadCiclos);
+  serialShow("Ciclos: ");
+  serialShow(String(cantidadCiclos));
   
-  leerPots();
 }
 void leerPots()
 {
@@ -165,7 +288,6 @@ void leerPots()
     else if(temp > 10 & temp<900) sel = 0;
     else sel = 2;
 }
-
 void calcularTiempos()
 {
   // 19 de agosto de 2020
@@ -185,12 +307,20 @@ void alejarseFinalCarrera()
     stepper.setSpeed(motorSpeed);
     stepper.runSpeed();
     delay(delayDriverResponse);
-    Serial.print("Alejandome del final de carrera...");
-    //Serial.println(stepper.currentPosition());
+    serialShow("Alejandome del final de carrera...");
+    //serialShowln(stepper.currentPosition());
   }
 }
 
 bool z0reached()
 {
   return !digitalRead(minZendstop);
+}
+void serialShow(String msg)
+{ 
+  if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
+    {  
+      Serial.println(msg);
+      xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
+    }
 }
